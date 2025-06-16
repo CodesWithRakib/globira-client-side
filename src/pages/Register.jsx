debfddd3d07ac5fb00c1a7745b663544457c0e52
Md. Rakib Islam <r.s.rakib.business@gmail.com>
2025-06-16 16:23:31 +0600

    register page updated with custom password validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,76 +1,71 @@
-import React, { use } from "react";
+import React from "react";
 import AuthImagePattern from "../components/AuthImagePattern";
 import { Link, useLocation, useNavigate } from "react-router";
 import { Loader2, EyeOffIcon, EyeIcon, GlobeLock } from "lucide-react";
-import { AuthContext } from "../Auth/AuthProvider";
 import toast from "react-hot-toast";
 import useAxios from "../hooks/useAxios";
 import useTitle from "../hooks/useTitle";
+import useAuth from "../hooks/useAuth";
 
 const Register = () => {
   const [isSigningUp, setIsSigningUp] = React.useState(false);
   const [showPassword, setShowPassword] = React.useState(false);
 
-  const { createUser, updateUser, setUser } = use(AuthContext);
-
+  const { createUser, updateUser, setUser } = useAuth();
   const axiosSecure = useAxios();
   const navigate = useNavigate();
   const { state } = useLocation();
 
   useTitle(`Register`);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
     const form = event.target;
     const formData = new FormData(form);
     const userInfo = Object.fromEntries(formData.entries());
-    const { email, password, ...others } = userInfo;
-    const { name, photoURL } = others;
+    const { email, password, name, photoURL } = userInfo;
 
-    setIsSigningUp(true);
-    createUser(email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        setUser(user);
-        toast.success("Account created successfully!");
-        axiosSecure.post("/api/users", { name, email, password, photoURL });
-        updateUser({ displayName: name, photoURL: photoURL })
-          .then(() => {
-            toast.success("Account updated successfully!");
-            console.log("user updated");
-          })
-          .catch((error) => {
-            toast.error("Account update failed!");
-            console.log(error);
-          });
-        form.reset();
+    // ✅ Custom password validation
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+    if (!passwordPattern.test(password)) {
+      toast.error(
+        "Password must contain uppercase, lowercase, digit, and be at least 6 characters."
+      );
+      return;
+    }
 
-        setTimeout(() => {
-          navigate(`${state ? state : "/"}`);
-        }, 2000);
-      })
-      .catch((error) => {
-        toast.error("Account creation failed!");
-        console.log(error);
-      })
-      .finally(() => {
-        setIsSigningUp(false);
-      });
+    try {
+      setIsSigningUp(true);
+      const userCredential = await createUser(email, password);
+      const user = userCredential.user;
+      setUser(user);
+
+      await updateUser({ displayName: name, photoURL });
+      toast.success("Account created & updated successfully!");
+
+      // ✅ Save user to DB
+      await axiosSecure.post("/api/users", { name, email, password, photoURL });
+
+      form.reset();
+
+      setTimeout(() => {
+        navigate(state || "/");
+      }, 2000);
+    } catch (error) {
+      toast.error(error.message || "Account creation failed!");
+    } finally {
+      setIsSigningUp(false);
+    }
   };
+
   return (
     <div className="min-h-screen grid lg:grid-cols-2">
       {/* left side */}
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
-          {/* LOGO */}
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
-              <div
-                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
-              group-hover:bg-primary/20 transition-colors"
-              >
+              <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
                 <GlobeLock className="size-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Create Account</h1>
@@ -104,26 +99,22 @@ const Register = () => {
                 <input
                   type="text"
                   name="name"
-                  className={`  w-full `}
-                  placeholder="Md.Rakib Islam"
                   required
+                  placeholder="Md.Rakib Islam"
+                  className="w-full"
                 />
               </label>
-              <div className="validator-hint hidden">Enter valid name</div>
             </div>
+
             <div className="form-control">
               <p className="label-text font-medium">PhotoURL</p>
               <label className="input validator w-full">
                 <svg
+                  className="h-[2em] opacity-50"
                   xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
                   fill="none"
-                  className="h-[2em] opacity-50 lucide lucide-image-icon lucide-image "
+                  viewBox="0 0 24 24"
                   stroke="currentColor"
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
                   strokeWidth="2.5"
                 >
                   <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
@@ -133,13 +124,13 @@ const Register = () => {
                 <input
                   type="text"
                   name="photoURL"
-                  className={`  w-full `}
-                  placeholder="https://example.com/image.png"
                   required
+                  placeholder="https://example.com/photo.png"
+                  className="w-full"
                 />
               </label>
-              <div className="validator-hint hidden">Enter valid photoURL</div>
             </div>
+
             <div className="form-control">
               <p className="label-text font-medium">Email</p>
               <label className="input validator w-full">
@@ -162,20 +153,16 @@ const Register = () => {
                 <input
                   type="email"
                   name="email"
-                  className={`  w-full `}
-                  placeholder="mail@site.com"
                   required
+                  placeholder="mail@example.com"
+                  className="w-full"
                 />
               </label>
-              <div className="validator-hint hidden">
-                Enter valid email address
-              </div>
             </div>
 
             <div className="form-control">
               <p className="label-text font-medium">Password</p>
-
-              <label className="input validator w-full">
+              <label className="input validator w-full relative">
                 <svg
                   className="h-[2em] opacity-50"
                   xmlns="http://www.w3.org/2000/svg"
@@ -188,13 +175,8 @@ const Register = () => {
                     fill="none"
                     stroke="currentColor"
                   >
-                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
-                    <circle
-                      cx="16.5"
-                      cy="7.5"
-                      r=".5"
-                      fill="currentColor"
-                    ></circle>
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z" />
+                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />
                   </g>
                 </svg>
                 <input
@@ -202,14 +184,12 @@ const Register = () => {
                   name="password"
                   required
                   placeholder="Password"
-                  minLength="8"
-                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                  title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                  className="w-full"
                 />
                 <button
                   type="button"
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
                   onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center"
                 >
                   {showPassword ? (
                     <EyeIcon className="size-5 text-base-content/40" />
@@ -228,7 +208,7 @@ const Register = () => {
               {isSigningUp ? (
                 <>
                   <Loader2 className="h-5 w-5 animate-spin" />
-                  Loading...
+                  Creating...
                 </>
               ) : (
                 "Create Account"
@@ -248,11 +228,9 @@ const Register = () => {
       </div>
 
       {/* right side */}
-
       <AuthImagePattern
         title="Start Your Business Journey"
-        subtitle="Create your free account to connect with verified buyers and suppliers.
-Whether you’re sourcing or selling—your global growth starts here."
+        subtitle="Create your free account to connect with verified buyers and suppliers. Whether you’re sourcing or selling—your global growth starts here."
       />
     </div>
   );
